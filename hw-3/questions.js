00f6529f890ea15bc8e95cfb27fd8603e809a0f9
TijanaTanaskovic <51413403+TijanaTanaskovic@users.noreply.github.com>
2020-12-25 14:06:12 +0100

    ubacena pitanja, ponude odgovora i tacni odgovori na sva pitanja

--- hw-3/questions.js
+++ hw-3/questions.js
@@ -1,53 +1,53 @@
 var questions = [
     {
-        title: "",
-        choices: ["", "", "", ""],
-        answer: "alerts"
+        title: "Ko je autor knjige Ubiti pticu rugalicu?",
+        choices: ["Džordan Belfort", "Tolstoj", "Harper Li", "Olivera Ptica"],
+        answer: "Harper Li"
     },
     {
-        title: "",
-        choices: ["", "", "", ""],
-        answer: "parentheses"
+        title: "Knjige koje autorke su najprodavanije na svetu?",
+        choices: ["Agata Kristi", "Harper Li", "J.K Rouling", "Desanka Maksimović"],
+        answer: "Agata Kristi"
     },
     {
-        title: "",
-        choices: ["", "", "", ""],
-        answer: "parentheses"
+        title: "Čovek koji je rukopis knjige Zašto se niste ubili poneo sa sobom u logor Aušvic?",
+        choices: ["Bernard Šo", "Den Braun", "Viktor Frankl", "Dostojevski"],
+        answer: "Viktor Frankl"
     },
     
     {
-        title: "",
-        choices: ["", "", "", ""],
-        answer: ""
+        title: "Koji autor je dobio Nobelovu nagradu za književnost 1946. godine?",
+        choices: ["Ivo Andrić", "Herman Hese", "Miroslav Krleža", "Kurt Vonegat"],
+        answer: "Herman Hese"
     },
     {
-        title: "",
-        choices: ["", "", "", ""],
-        answer: ""
+        title: "Jedini dobitnik Pulicerove nagrade srpskog porekla je...",
+        choices: ["Mihajlo Pupin", "Nikola Tesla", "Dobrica Ćosić", "Milutin Milanković"],
+        answer: "Mihajlo Pupin"
     },
     {
-        title: "",
-        choices: ["", "", "", ""],
-        answer: ""
+        title: "Knjigu SHOE DOG napisao je osnivač koje sportske kompanije?",
+        choices: ["Adidas, Adolf Dasler", "Najki, Fil Najt", "Najki, Bil Bouerman", "Asiks, Onicuka"],
+        answer: "Najki, Fil Najt"
     },
     {
-        title: "",
-        choices: ["", "", "", ""],
-        answer: "parentheses"
+        title: "Koja od sledećih knjiga je iskorišćena kao materijal za film?",
+        choices: ["Poreklo - Den Braun", "Da Vinčijev kod - Den Braun", "Na Drini Ćuprija - Ivo Andrić", "Novi Jerusalim - Borislav Pekić"],
+        answer: "Da Vinčijev kod - Den Braun"
     },
     {
-        title: "",
-        choices: ["", "", "", ""],
-        answer: "parentheses"
+        title: "Ivo Andrić je odbio da se ekranizuje koja njegova knjiga?",
+        choices: ["Prokleta Avlija", "Znakovi pored puta", "Gospođica", "Kuća na Osami"],
+        answer: "Gospođica"
     },
     {
-        title: "",
-        choices: ["", "", "", ""],
-        answer: "parentheses"
+        title: "Dejan Stojiljković je knjigu Kainov ožiljak napisao zajedno sa...?",
+        choices: ["Vladimirom Kecmanovićem", "Dejanom Lučićem", "Dejan Tiagom Stankovićem", "Vanjom Nikolić"],
+        answer: "Vladimirom Kecmanovićem"
     },
     {
-        title: "",
-        choices: ["", "", "", ""],
-        answer: ""
+        title: "Autor knjige Kaluđer koji je prodao svoj Ferari je?",
+        choices: ["Toni Robins", "Smiljan Mori", "Robin Šarma", "Džim Kvik"],
+        answer: "Robin Šarma"
     }
 ];
