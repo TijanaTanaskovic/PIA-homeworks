967efc0f4fed5fc7eb6db99dea381db30a3f806f
TijanaTanaskovic <51413403+TijanaTanaskovic@users.noreply.github.com>
2020-12-25 12:45:36 +0100

    kreiranje skripte sa pitanjima; svako pitanje imas title, tj. kako ono glasi, potom choices, tj. izbor korisniku da izabere i answer, tj. tacan odgovor na samo pitanje

---
+++ hw-3/questions.js
@@ -0,0 +1,53 @@
+var questions = [
+    {
+        title: "",
+        choices: ["", "", "", ""],
+        answer: "alerts"
+    },
+    {
+        title: "",
+        choices: ["", "", "", ""],
+        answer: "parentheses"
+    },
+    {
+        title: "",
+        choices: ["", "", "", ""],
+        answer: "parentheses"
+    },
+    
+    {
+        title: "",
+        choices: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        title: "",
+        choices: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        title: "",
+        choices: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        title: "",
+        choices: ["", "", "", ""],
+        answer: "parentheses"
+    },
+    {
+        title: "",
+        choices: ["", "", "", ""],
+        answer: "parentheses"
+    },
+    {
+        title: "",
+        choices: ["", "", "", ""],
+        answer: "parentheses"
+    },
+    {
+        title: "",
+        choices: ["", "", "", ""],
+        answer: ""
+    }
+];
