656c884c47910504a897de71d5e5a9edaef973c6
TijanaTanaskovic <tijana.kg98@gmail.com>
2021-01-31 00:23:49 +0100

    dodati filmovi iz baze i mogucnost biranja po zanrovima
    dodatna podesavanja u izgledu nekih delova, sredjena home.php strane

--- hw-4/home.php
+++ hw-4/home.php
@@ -31,31 +31,73 @@ Domaci 4 (PIA 2020/2021)
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-         Genres
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Comedy</a>
-          <a class="dropdown-item" href="#">Drama</a>
-        </div>
-      </li>
+      <form action="home.php" method="post">
+      <label for="genre"><p class="problem1">Choose a genre:</p></label>
+      <select name="genre" id="genre">
+      <option value="action">Action</option>
+      <option value="comedy">Comedy</option>
+      <option value="drama">Drama</option>
+      <option value="thriller">Thriller</option>
+      <option value="horror">Horror</option>
+      <option value="romance">Romance</option>
+      </select>
+      <br><br>
+      <input type="submit" class="btn btn-outline-light" value="Submit">
+    </form>
     </ul>
     
-    <form class="navbar-form navbar-right">
-            <input type="text" class="form-control mr-sm-2" placeholder="Search">
+    <form class="navbar-form navbar-right" action="home.php" method="post">
+            <input type="text" class="form-control mr-sm-2" placeholder="Search for a movie" name="find">
             <button type="submit" class="btn btn-outline-light">Search</button>
     </form>
     <div class="problem">
     <form class="navbar-form navbar-right">
     <a class="btn btn-outline-light" href="login.php">Log Out</a>
     </form>
-</div>
+  </div>
   </div>
 </nav>
+
+<?php
+    
+    session_start();
+
+    $con = mysqli_connect('localhost', 'root', '');
+
+    mysqli_select_db($con, 'imdb');
+
+    if(isset($_POST['find'])) {
+    $search = ($_POST['find']);
+    $check = "SELECT * FROM movies WHERE name LIKE '%$search%'";
+    }
+    else if(isset($_POST['genre'])) {
+    $search = ($_POST['genre']);
+    $check = "SELECT * FROM movies WHERE genre LIKE '%$search%'";
+    }
+    else{
+    $check = "SELECT * FROM movies";
+    }
+
+    $result = mysqli_query($con, $check);
+    $resultCheck = mysqli_num_rows($result);
+    if($resultCheck > 0){
+        while($row = mysqli_fetch_assoc($result)){
+        
+?>
+    <div class="container">
+        <div class="content">
+            <img class="movie-photo" src="<?= $row['photo'] ?>">
+            <div class="title">
+            <p class="title"> <?php echo $row['name']; ?></p> <?php echo "</a>"; ?>
+            </div>
+        </div>
+    </div>
+<?php
+  }}
+?>
+
 
 <footer class="container-fluid text-center">
   <div class="row py-4 d-flex align-items-center">
